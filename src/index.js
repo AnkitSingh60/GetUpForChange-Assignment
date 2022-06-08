fea31d2ec2d9559270deaa80342b62a61cb184fb
AnkitSingh60 <ankitanwar730@gmail.com>
2022-06-08 11:13:03 +0530

    added routes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require('dotenv');
 const cors = require("cors");
 const connect = require("./configs/db")
+const {login, register} = require("./controller/user.controller")
 
 const app = express();
 
@@ -11,6 +12,9 @@ app.use(express.json())
 
 const PORT = process.env.PORT || 5000;
 app.use(cors());
+
+app.post("/register",register)
+app.post("/login",login)
 
 app.listen(PORT, async () => {
     try {
