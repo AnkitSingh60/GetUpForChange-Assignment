a2038d3f4dc897b7155ff66900820dacdfd780fb
AnkitSingh60 <ankitanwar730@gmail.com>
2022-06-08 11:31:12 +0530

    added homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css';
+import Home from './components/Home'
 
 function App() {
   return (
     <div className="App">
-      <h1>Heyy kiddo</h1>
+      <>
+      <Home />
+      </>
     </div>
   );
 }
