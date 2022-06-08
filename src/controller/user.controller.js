b90252ac08e164d667548e46c9b99c3fca7007f6
AnkitSingh60 <ankitanwar730@gmail.com>
2022-06-08 11:02:22 +0530

    added user login method

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -18,3 +18,24 @@ const register = async (req,res) => {
         console.log('error:', error)
     }
 }
+
+const login = async (req,res) => {
+    try {
+        // we will try to find the user with the email provided
+        const user = await User.findOne({email: req.body.email})
+
+        // if user is not found then return error
+        if(!user) return res.send({Message: "Either the email or the password you entered is not correct"})
+
+        // if user is found then  we will match the password
+        const match = user.checkPassword(req.body.password) 
+
+        if(!match) return res.status(400).send({Message: "Either the email or the password you entered is not correct"}) 
+
+        res.send(user)
+    } catch (error) {
+        console.log('error:', error)
+    }
+}
+
+module.exports ={register,login}
