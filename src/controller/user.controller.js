fac83f14fc4c5921220cf37855c7dace19a91af7
AnkitSingh60 <ankitanwar730@gmail.com>
2022-06-08 11:00:34 +0530

    added user register method

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,1 +1,20 @@
 const User = require("../model/user.model")
+
+const register = async (req,res) => {
+    try {
+         // first find the user
+         let user = await User.findOne({email: req.body.email}).lean().exec();
+
+          // if user is found then give error
+        if(user) 
+        return res.status(400).send({Message:"Please another email"})
+
+        // if user is not found thn create user with data provided
+        user = await User.create(req.body);
+
+        res.send(user)
+
+    } catch (error) {
+        console.log('error:', error)
+    }
+}
